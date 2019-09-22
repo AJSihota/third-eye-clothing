7dc02c3f061c94c18ecc5288715fbb00ced74991
user <user@users-MacBook-Pro-2.local>
2019-09-22 16:41:39 -0600

    Initial deployment

---
+++ src/homepage.component.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+const HomePage = () => {
+return(
+<div className="homepage">
+    <div className="directory-menu">
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">Hats</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">T-Shirts</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">Sweaters</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+
+        <div className="menu-item">
+            <div className="content">
+                <h1 className="title">Misc</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
+        </div>
+    </div>
+</div>
+);}
+
+export default HomePage;
