2c2afa3639b23e3de97aaf78211cabe8b08f2356
user <user@users-MacBook-Pro-2.local>
2019-10-06 17:46:42 -0600

    broke down homepage into smaller components, finished styling menu item component

--- src/pages/homepage.component.jsx
+++ src/pages/homepage.component.jsx
@@ -1,37 +1,12 @@
 import React from 'react';
+import Directory from '../components/directory/directory.component';
 
 const HomePage = () => {
 return(
 <div className="homepage">
-    <div className="directory-menu">
-        <div className="menu-item">
-            <div className="content">
-                <h1 className="title">Hats</h1>
-                <span className="subtitle">SHOP NOW</span>
-            </div>
-        </div>
+    
+<Directory />
 
-        <div className="menu-item">
-            <div className="content">
-                <h1 className="title">T-Shirts</h1>
-                <span className="subtitle">SHOP NOW</span>
-            </div>
-        </div>
-
-        <div className="menu-item">
-            <div className="content">
-                <h1 className="title">Sweaters</h1>
-                <span className="subtitle">SHOP NOW</span>
-            </div>
-        </div>
-
-        <div className="menu-item">
-            <div className="content">
-                <h1 className="title">Misc</h1>
-                <span className="subtitle">SHOP NOW</span>
-            </div>
-        </div>
-    </div>
 </div>
 );}
 
